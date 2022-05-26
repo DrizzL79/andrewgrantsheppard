d5b3a2e5ea3cfb0021b4c97bd86e9e0e3445336c
Andrew Grant Sheppard <grantsheppard5@gmail.com>
2022-05-26 03:58:48 -0400

    update projects.js

--- projects.js
+++ projects.js
@@ -1,10 +1,19 @@
 const para = document.querySelector('button');
+const div = document.querySelector('div');
+const p = document.createElement('p');
 
 para.onclick = getTime;
 
 function printTime() {
-    var d = new Date();
-    para.textContent = `${d}`;
+    let d = new Date();
+    p.textContent = `${d}`;
+    div.appendChild(p);
+    p.style.color = "white";
+    p.style.backgroundColor = "black";
+    p.style.width = "300px";
+    p.style.borderRadius = "15px";
+    p.style.textAlign = "center";
+    p.style.margin = "auto";
 } 
 
 function getTime() {
